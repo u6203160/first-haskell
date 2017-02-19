6d5ddba24a6db38b62433433fe7e4b5ab9b89d8b
Patrick <patricktran159@gmail.com>
2017-02-19 19:33:05 +1100

    string

--- _solve_quadratic.hs
+++ _solve_quadratic.hs
@@ -1,6 +1,6 @@
--- for students v2
+-- for students v3
 module Solve_Quadratic
     where
 
-solve_quadratic :: Float -> Float -> Float -> Int -> Int
+solve_quadratic :: Float -> Float -> Float -> String
 solve_quadratic a b c = 0.0
