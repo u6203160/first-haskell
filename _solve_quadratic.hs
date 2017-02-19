e810c362b1fb12e40f89e569ab2f08aa4117420a
Patrick <patricktran159@gmail.com>
2017-02-19 18:49:53 +1100

    update type Int

--- _solve_quadratic.hs
+++ _solve_quadratic.hs
@@ -1,6 +1,6 @@
--- for students
+-- for students v2
 module Solve_Quadratic
     where
 
-solve_quadratic :: Float -> Float -> Float -> Float
+solve_quadratic :: Float -> Float -> Float -> Int -> Int
 solve_quadratic a b c = 0.0
